e02130127c4ea4280c14c7c8bba9a79f3acf3d22
Matthias Springer <me@matthiasspringer.de>
2016-10-22 18:48:11 +0900

    CUDA path auto configuration.

--- lib/config/os_configuration.rb
+++ lib/config/os_configuration.rb
@@ -1,9 +1,15 @@
 require "rbconfig"
+require "logger"
 
 module Ikra
+    Log = Logger.new(STDOUT)
+
     module Configuration
         class << self
             SUPPORTED_OS = [:linux, :macosx]
+            ATTEMPT_AUTO_CONFIG = true
+
+            # These values are used only if auto configuration fails
             CUDA_NVCC = "/usr/local/cuda-8.0/bin/nvcc"
             CUDA_COMMON_INCLUDE = "/home/matthias/NVIDIA_CUDA-7.5_Samples/common/inc"
             CUDA_CUPTI_INCLUDE = "/usr/local/cuda-7.5/extras/CUPTI/include"
@@ -13,23 +19,43 @@ module Ikra
                     raise "Operating system not supported: #{operating_system}"
                 end
 
+                @@cuda_nvcc = CUDA_NVCC
+                @@cuda_common_include = CUDA_COMMON_INCLUDE
+                @@cuda_cupti_include = CUDA_CUPTI_INCLUDE
+
+                # Auto configuration
+                if ATTEMPT_AUTO_CONFIG
+                    Log.info("Attempting CUDA path auto configuration")
+
+                    nvcc_path = %x(which nvcc)
+
+                    if $?.exitstatus == 0
+                        cuda_path = File.expand_path("../..", nvcc_path)
+                        @@cuda_nvcc = File.expand_path("bin/nvcc", cuda_path)
+                        @@cuda_common_include = File.expand_path("samples/common/inc", cuda_path)
+                        @@cuda_cupti_include = File.expand_path("extras/CUPTI/include", cuda_path)
+                    else
+                        Log.warn("CUDA path auto configuration failed")
+                    end
+                end
+
                 # Check if nvcc is installed
-                %x(#{CUDA_NVCC})
+                %x(#{@@cuda_nvcc})
                 if $?.exitstatus != 1
                     raise "nvcc not installed"
                 end
 
-                if !File.directory?(CUDA_COMMON_INCLUDE)
-                    raise "Directory does not exist: #{CUDA_COMMON_INCLUDE}. Check OS configuration!"
+                if !File.directory?(@@cuda_common_include)
+                    raise "Directory does not exist: #{@@cuda_common_include}. Check OS configuration!"
                 end
 
-                if !File.directory?(CUDA_CUPTI_INCLUDE)
-                    raise "Directory does not exist: #{CUDA_CUPTI_INCLUDE}. Check OS configuration!"
+                if !File.directory?(@@cuda_cupti_include)
+                    raise "Directory does not exist: #{@@cuda_cupti_include}. Check OS configuration!"
                 end
             end
 
             def nvcc_invocation_string(in_file, out_file)
-                "#{CUDA_NVCC} -o #{out_file} -I#{CUDA_COMMON_INCLUDE} -I#{CUDA_CUPTI_INCLUDE} --shared -Xcompiler -fPIC #{in_file}"
+                "#{@@cuda_nvcc} -o #{out_file} -I#{@@cuda_common_include} -I#{@@cuda_cupti_include} --shared -Xcompiler -fPIC #{in_file}"
             end
 
             def so_suffix
