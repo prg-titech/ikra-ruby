aac4f919b079f8bbf6ca9b3b01a9840678806a9c
Peter Wauligmann <peter.wauligmann@gmail.com>
2016-10-27 19:07:09 +0900

    acos added to math

--- lib/ruby_core/math.rb
+++ lib/ruby_core/math.rb
@@ -1,8 +1,9 @@
 module Ikra
     module RubyIntegration
-        implement Math.singleton_class, :cos, FLOAT, 1, "cos((float) #1)", pass_self: false
-        implement Math.singleton_class, :sin, FLOAT, 1, "sin((float) #1)", pass_self: false
-        implement Math.singleton_class, :tan, FLOAT, 1, "tan((float) #1)", pass_self: false
+        implement Math.singleton_class, :cos, FLOAT, 1, "cosf((float) #1)", pass_self: false
+        implement Math.singleton_class, :sin, FLOAT, 1, "sinf((float) #1)", pass_self: false
+        implement Math.singleton_class, :tan, FLOAT, 1, "tanf((float) #1)", pass_self: false
+        implement Math.singleton_class, :acos, FLOAT, 1, "acosf((float) #1)", pass_self: false
     end
 end
 
