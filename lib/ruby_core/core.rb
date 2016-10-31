33f2c586f7c266c32d5bbffe879eaf0ba66ad884
Peter Wauligmann <peter.wauligmann@gmail.com>
2016-10-31 18:26:41 +0900

    added math and fixnum operations

--- lib/ruby_core/core.rb
+++ lib/ruby_core/core.rb
@@ -39,6 +39,7 @@ module Ikra
         # TODO: fix int % float
         implement Fixnum, :%, PROMOTE_TO_FIXNUM_FLOAT, 1, "(#1 % #2)"
         implement Fixnum, :&, ASSERT_AND_RETURN_FIXNUM, 1, "(#1 & #2)"
+        implement Fixnum, :|, ASSERT_AND_RETURN_FIXNUM, 1, "(#1 | #2)"
         implement Fixnum, :*, PROMOTE_TO_FIXNUM_FLOAT, 1, "(#1 * #2)"
         # TODO: Implement integer pow
         implement Fixnum, :**, FLOAT, 1, "powf((float) #1, (float) #2)"
@@ -57,6 +58,18 @@ module Ikra
         implement Fixnum, :>>, ASSERT_AND_RETURN_FIXNUM, 1, "(#1 >> #2)"
         implement Fixnum, :"^", ASSERT_AND_RETURN_FIXNUM, 1, "(#1 ^ #2)"
 
+	implement Fixnum, :abs, INT, 0, "(#1 < 0 ? -(#1) : #1)"
+	implement Fixnum, :bit_length, INT, 1, "((int) (ceil(log2f((#1) < 0 ? -(#1) : (#1) + 1))))"
+	implement Fixnum, :div, INT, 1, "((int) (#1 / #2))"
+	implement Fixnum, :even?, BOOL, 0, "(#1 % 2 == 0)"
+	implement Fixnum, :fdiv, FLOAT, 1, "(#1 / ((float) #2))"
+	implement Fixnum, :magnitude, INT, 0, "(#1 < 0 ? -(#1) : #1)"
+        implement Fixnum, :modulo, PROMOTE_TO_FIXNUM_FLOAT, 1, "(#1 % #2)"
+	implement Fixnum, :odd?, BOOL, 0, "(#1 % 2 != 0)"
+	implement Fixnum, :size, INT, 0, "(sizeof(int))"
+	implement Fixnum, :next, INT, 0, "(#1 + 1)"
+	implement Fixnum, :succ, INT, 0, "(#1 + 1)"
+	implement Fixnum, :zero?, BOOL, 0, "(#1 == 0)"
         implement Fixnum, :to_f, FLOAT, 1, "(float) #1"
         implement Fixnum, :to_i, INT, 1, "#1"
 
--- test/unit/math_test.rb
+++ test/unit/math_test.rb
@@ -17,4 +17,196 @@ class MathTest < UnitTestCase
 
         assert_in_delta(100*1.266104, array.reduce(:+), 0.01)
     end
+
+    def test_acosh
+        array = Array.pnew(100) do |j|
+            Math.acosh(5) + 0
+        end
+
+        assert_in_delta(100*2.29243166956, array.reduce(:+), 0.01)
+    end
+
+    def test_asin
+        array = Array.pnew(100) do |j|
+            Math.asin(0.3) + 0
+        end
+
+        assert_in_delta(100*0.304693, array.reduce(:+), 0.01)
+    end
+
+    def test_asinh
+        array = Array.pnew(100) do |j|
+            Math.asinh(5) + 0
+        end
+
+        assert_in_delta(100*2.31243834, array.reduce(:+), 0.01)
+    end
+
+    def test_atan
+        array = Array.pnew(100) do |j|
+            Math.atan(0.3) + 0
+        end
+
+        assert_in_delta(100*0.291456, array.reduce(:+), 0.01)
+    end
+
+    def test_atan2
+        array = Array.pnew(100) do |j|
+            Math.atan2(0.2, 0.5) + 0
+        end
+
+        assert_in_delta(100*0.380506, array.reduce(:+), 0.01)
+    end
+
+    def test_atanh
+        array = Array.pnew(100) do |j|
+            Math.atanh(0.5) + 0
+        end
+
+        assert_in_delta(100*0.549306, array.reduce(:+), 0.01)
+    end
+
+    def test_cbrt
+        array = Array.pnew(100) do |j|
+            Math.cbrt(0.3) + 0
+        end
+
+        assert_in_delta(100*0.669433, array.reduce(:+), 0.01)
+    end
+
+    def test_cos
+        array = Array.pnew(100) do |j|
+            Math.cos(2) + 0
+        end
+
+        assert_in_delta(100*-0.41614683, array.reduce(:+), 0.01)
+    end
+
+    def test_cosh
+        array = Array.pnew(100) do |j|
+            Math.cosh(0.3) + 0
+        end
+
+        assert_in_delta(100*1.045339, array.reduce(:+), 0.01)
+    end
+
+    def test_erf
+        array = Array.pnew(100) do |j|
+            Math.erf(0.3) + 0
+        end
+
+        assert_in_delta(100*0.328627, array.reduce(:+), 0.01)
+    end
+
+    def test_erfc
+        array = Array.pnew(100) do |j|
+            Math.erfc(0.3) + 0
+        end
+
+        assert_in_delta(100*0.671373, array.reduce(:+), 0.01)
+    end
+
+    def test_exp
+        array = Array.pnew(100) do |j|
+            Math.exp(0.3) + 0
+        end
+
+        assert_in_delta(100*1.349859, array.reduce(:+), 0.01)
+    end
+
+    def test_gamma
+        array = Array.pnew(100) do |j|
+            Math.gamma(0.3) + 0
+        end
+
+        assert_in_delta(100*2.99157, array.reduce(:+), 0.01)
+    end
+
+    def test_hypot
+        array = Array.pnew(100) do |j|
+            Math.hypot(0.3, 0.5) + 0
+        end
+
+        assert_in_delta(100*0.583095, array.reduce(:+), 0.01)
+    end
+
+    def test_ldexp
+        array = Array.pnew(100) do |j|
+            Math.ldexp(0.3, 5) + 0
+        end
+
+        assert_in_delta(100*9.6, array.reduce(:+), 0.01)
+    end
+
+    def test_lgamma
+        array = Array.pnew(100) do |j|
+            Math.lgamma(0.3) + 0
+        end
+
+        assert_in_delta(100*1.09580, array.reduce(:+), 0.01)
+    end
+
+    def test_log
+        array = Array.pnew(100) do |j|
+            Math.log(0.3) + 0
+        end
+
+        assert_in_delta(100*-1.20397, array.reduce(:+), 0.01)
+    end
+
+    def test_log10
+        array = Array.pnew(100) do |j|
+            Math.log10(0.3) + 0
+        end
+
+        assert_in_delta(100*-0.522879, array.reduce(:+), 0.01)
+    end
+
+    def test_log2
+        array = Array.pnew(100) do |j|
+            Math.log2(0.3) + 0
+        end
+
+        assert_in_delta(100*-1.73697, array.reduce(:+), 0.01)
+    end
+
+    def test_sin
+        array = Array.pnew(100) do |j|
+            Math.sin(0.3) + 0
+        end
+
+        assert_in_delta(100*0.295520, array.reduce(:+), 0.01)
+    end
+
+    def test_sinh
+        array = Array.pnew(100) do |j|
+            Math.sinh(5) + 0
+        end
+
+        assert_in_delta(100*74.2032105, array.reduce(:+), 0.01)
+    end
+
+    def test_sqrt
+        array = Array.pnew(100) do |j|
+            Math.sqrt(0.3) + 0
+        end
+
+        assert_in_delta(100*0.547723, array.reduce(:+), 0.01)
+    end
+
+    def test_tan
+        array = Array.pnew(100) do |j|
+            Math.tan(0.3) + 0
+        end
+
+        assert_in_delta(100*0.309336, array.reduce(:+), 0.01)
+    end
+
+    def test_tanh
+        array = Array.pnew(100) do |j|
+            Math.tanh(0.3) + 0
+        end
+
+        assert_in_delta(100*0.291313, array.reduce(:+), 0.01)
+    end
 end
