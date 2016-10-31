17188a829ccc2882ee639da3a446370b3d2a0c26
Matthias Springer <me@matthiasspringer.de>
2016-10-31 15:29:42 +0900

    Add unit test for union type method invocation on Fixum, Float.

--- test/unit/type_inference_test.rb
+++ test/unit/type_inference_test.rb
@@ -4,7 +4,7 @@ require_relative "unit_test_template"
 class TypeInferenceTest < UnitTestCase
     def test_simple_type_inference
         # Only one (linear) pass required
-        base_array = Array.pnew(100) do |j|
+        array = Array.pnew(100) do |j|
             x1 = j * 2
             x2 = x1 % 2 == 0
 
@@ -15,12 +15,12 @@ class TypeInferenceTest < UnitTestCase
             end
         end
 
-        assert_equal(9900, base_array.reduce(:+))
+        assert_equal(9900, array.reduce(:+))
     end
 
     def test_type_inference_assign_int_float
         # Only one (linear) pass required
-        base_array = Array.pnew(100) do |j|
+        array = Array.pnew(100) do |j|
             x1 = j
 
             if j%2 == 0
@@ -30,7 +30,7 @@ class TypeInferenceTest < UnitTestCase
             x1
         end
 
-        assert_in_delta(4950, base_array.reduce(:+), 0.001)
+        assert_in_delta(4950, array.reduce(:+), 0.001)
 
         for index in 0...100
             if index % 2 == 0
@@ -39,7 +39,31 @@ class TypeInferenceTest < UnitTestCase
                 expected_type = ::Fixnum
             end
 
-            assert_equal(base_array[index].class, expected_type)
+            assert_equal(array[index].class, expected_type)
+        end
+    end
+
+    def test_union_type_primitive_invocation
+        array = Array.pnew(100) do |j|
+            x1 = j
+
+            if j%2 == 0
+                x1 = 2 * j.to_f
+            end
+
+            x1 * 2
+        end
+
+        assert_in_delta(7400 * 2, array.reduce(:+), 0.001)
+
+        for index in 0...100
+            if index % 2 == 0
+                expected_type = ::Float
+            else
+                expected_type = ::Fixnum
+            end
+
+            assert_equal(array[index].class, expected_type)
         end
     end
 end
