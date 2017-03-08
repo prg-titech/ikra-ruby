b98c168721b24406220dbcf450fd32c2302ae2aa
Matthias Springer <me@matthiasspringer.de>
2017-03-08 17:07:18 +0900

    Add masks to image library.

--- test/interactive/image_library/test/playground.rb
+++ test/interactive/image_library/test/playground.rb
@@ -4,7 +4,7 @@ require_relative "../image_library"
 tokyo = ImageLibrary::Core.load_image("tokyo.png")
 
 # Blur the image multiple times
-for i in 1..10
+for i in 0...3
     tokyo = tokyo.apply_filter(ImageLibrary::Filters.blur)
     #tokyo = tokyo.apply_filter(ImageLibrary::Filters.sharpen)
 end
@@ -13,9 +13,14 @@ end
 sunset = ImageLibrary::Core.load_image("sunset.png")
 combined = tokyo.apply_filter(ImageLibrary::Filters.blend(sunset, 0.3))
 
-#forest = ImageLibrary::Core.load_image("forest.png")
-#forest = forest.apply_filter(ImageLibrary::Filters.invert) 
+forest = ImageLibrary::Core.load_image("forest.png")
+forest = forest.apply_filter(ImageLibrary::Filters.invert) 
 #combined = combined.apply_filter(ImageLibrary::Filters.blend(forest, 0.1))
+
+combined = combined.apply_filter(ImageLibrary::Filters.overlay(
+    #ImageLibrary::Masks.rect(50, 0, 200, 20), forest))
+    ImageLibrary::Masks.circle(tokyo.height / 4), forest))
+    #ImageLibrary::Masks.mandelbrot, forest))
 
 # Show result
 combined.render
